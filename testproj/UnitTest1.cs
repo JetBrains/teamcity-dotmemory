007888557cd7f1cade28749bd7177782da49218f
Nikolay Pianikov <Nikolay.Pianikov@jetbrains.com>
2015-08-19 16:29:43 +0300

    Add test proj

---
+++ testproj/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace testproj
+{
+    using JetBrains.dotMemoryUnit;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            dotMemory.Check(
+                memory =>
+                {
+                    Assert.AreEqual(10, memory.ObjectsCount);
+                });
+        }
+    }
+}
