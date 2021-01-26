a048a65ec93a0280cec47d7de137fa177fcd3d45
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2021-01-26 14:02:23 +0300

    fix tests

--- dotMemoryUnit-agent/src/test/java/jetbrains/buildServer/dotMemoryUnit/agent/DotMemoryUnitProjectGeneratorTest.java
+++ dotMemoryUnit-agent/src/test/java/jetbrains/buildServer/dotMemoryUnit/agent/DotMemoryUnitProjectGeneratorTest.java
@@ -52,7 +52,7 @@ public class DotMemoryUnitProjectGeneratorTest {
   @Test
   public void shouldGenerateContent() {
     // Given
-    String expectedContent = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>" +
+    String expectedContent = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>" +
                              "<dotMemoryUnit>" +
                              "<TargetExecutable>" + "wd" + File.separator + "tool" + "</TargetExecutable>\n" +
                              "<TargetArguments>arg1 arg2</TargetArguments>" +
